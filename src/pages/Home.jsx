313c1ede8256df24ecb1ee27d5e16676e7fe26d9
Sandhya Rajwanshi <47480884+SandhyaR1007@users.noreply.github.com>
2024-12-08 13:59:53 +0530

    update: adjust Island lighting

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,21 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import Loader from "../components/Loader";
+import Island from "../models/Island";
 
 const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale,
+      screenPosition = [0, -6.5, -43],
+      rotation = [0.1, 4.7, 0];
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+    return [screenScale, screenPosition, rotation];
+  };
+  const [islandScale, islandPosition, rotation] = adjustIslandForScreenSize();
   return (
     <section className="w-full h-screen relative">
       <Canvas
@@ -10,11 +23,18 @@ const Home = () => {
         camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader />}>
-          <directionalLight />
-          <ambientLight />
-          <pointLight />
-          <spotLight />
-          <hemisphereLight />
+          <directionalLight position={[1, 1, 1]} intensity={2} />
+          <ambientLight intensity={0.5} />
+          <hemisphereLight
+            skyColor="#b1e1ff"
+            groundColor="#000000"
+            intensity={1}
+          />
+          <Island
+            scale={islandScale}
+            position={islandPosition}
+            rotation={rotation}
+          />
         </Suspense>
       </Canvas>
     </section>
